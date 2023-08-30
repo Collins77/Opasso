fab479e7711c32860f182af748f60614972cc44f
Collins77 <collinsmuemah@gmail.com>
2023-08-30 12:32:46 +0300

    updates

--- frontend/src/pages/SuppliersPage.jsx
+++ frontend/src/pages/SuppliersPage.jsx
@@ -28,7 +28,7 @@ const SuppliersPage = () => {
       <br />
       <br />
       <div className={`${styles.section}`}>
-        <div className="grid grid-cols-4 gap-[10px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+        <div className="grid grid-cols-4 gap-[25px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
           {sellers && sellers.map((i, index) => <SupplierCard data={i} key={index} />)}
         </div>
         {sellers && sellers.length === 0 ? (
