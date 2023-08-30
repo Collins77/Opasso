c8e5a1c8f2eb861b2de844fb0e128ea292dcbf5e
Collins77 <collinsmuemah@gmail.com>
2023-08-30 10:17:05 +0300

    updates

--- frontend/src/pages/SuppliersPage.jsx
+++ frontend/src/pages/SuppliersPage.jsx
@@ -29,13 +29,13 @@ const SuppliersPage = () => {
       <br />
       <div className={`${styles.section}`}>
         <div className="grid grid-cols-2 gap-[10px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-          {data && data.map((i, index) => <SupplierCard data={i} key={index} />)}
+          {sellers && sellers.map((i, index) => <SupplierCard data={i} key={index} />)}
         </div>
-        {data && data.length === 0 ? (
+        {/* {sellers && sellers.length === 0 ? (
           <h1 className="text-center w-full pb-[100px] text-[20px]">
             No Suppliers Found!
           </h1>
-        ) : null}
+        ) : null} */}
       </div>
       <Footer />
     </div>
