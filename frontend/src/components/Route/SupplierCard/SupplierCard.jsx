adc66e9258fcd38ee1fc69cb7b0062b39beb1b41
Collins77 <collinsmuemah@gmail.com>
2023-08-31 14:30:44 +0300

    updates

--- frontend/src/components/Route/SupplierCard/SupplierCard.jsx
+++ frontend/src/components/Route/SupplierCard/SupplierCard.jsx
@@ -12,7 +12,7 @@ const SupplierCard = ({ data,isEvent }) => {
               <a href="#">
                   <h5 className="mb-1 text-xl font-semibold tracking-tight text-blue-600 dark:text-white">{data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}</h5>
               </a>
-              <div className="mb-2 flex">
+              <div className="mb-2 flex justify-around">
                 <p className="text-green-600">Dollar Exchange Rate: <br /><span>KES. {data.exchangeRate}</span></p>
                 <p>Address: <span>{data.address}</span></p>
               </div>
