560e1478777d9a34bde79634a5454cb33c1573ec
Collins77 <collinsmuemah@gmail.com>
2023-08-31 12:53:02 +0300

    updates

--- frontend/src/components/Route/SupplierCard/SupplierCard.jsx
+++ frontend/src/components/Route/SupplierCard/SupplierCard.jsx
@@ -10,7 +10,7 @@ const SupplierCard = ({ data,isEvent }) => {
         <Link to={`/shop/preview/${data?._id}`}>
           
           <div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <img className="w-7 h-7 mb-3 rounded-full" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" src={`${data.avatar?.url}`}/>
+              <img className="w-15 h-15 mb-3 rounded-full" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" src={`${data.avatar?.url}`}/>
               <a href="#">
                   <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}</h5>
               </a>
