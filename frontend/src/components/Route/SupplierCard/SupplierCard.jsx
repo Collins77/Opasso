102a14b9a2b60f6e4252bffd939f9e34e629fc50
Collins77 <collinsmuemah@gmail.com>
2023-08-31 14:08:39 +0300

    updates

--- frontend/src/components/Route/SupplierCard/SupplierCard.jsx
+++ frontend/src/components/Route/SupplierCard/SupplierCard.jsx
@@ -6,17 +6,15 @@ const SupplierCard = ({ data,isEvent }) => {
 
   return (
     <>
-        {/* <div className="w-full h-[200px] rounded-lg shadow-sm p-3 relative cursor-pointer col-span-4"> */}
         <Link to={`/shop/preview/${data?._id}`}>
-          
           <div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <img className="w-8 h-8 mb-3 rounded-full" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" src={`${data.avatar?.url}`}/>
               <a href="#">
                   <h5 className="mb-2 text-xl font-semibold tracking-tight text-blue-600 dark:text-white">{data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}</h5>
               </a>
-              <div className="mb-3 flex mt-4 space-x-3 md:mt-6">
+              <div className="mb-3 flex mt-2 space-x-3 md:mt-6">
                 <p className="text-green-600">Dollar Exchange Rate: <span >KES. {data.exchangeRate}</span></p>
-                <p>Location: <span>{data.address}</span></p>
+                <p>Address: <span>{data.address}</span></p>
               </div>
               <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
                 <Link to={`/shop/preview/${data?._id}`} className="inline-flex items-center text-blue-600 hover:underline">
