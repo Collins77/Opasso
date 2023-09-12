1dddb4079f4cc0a8746541d29fe8ceac2f47ebeb
Collins77 <collinsmuemah@gmail.com>
2023-09-12 11:38:34 +0300

    updates

--- frontend/src/components/Shop/PreviewAllProducts.jsx
+++ frontend/src/components/Shop/PreviewAllProducts.jsx
@@ -1,24 +1,80 @@
 import React, { useState } from 'react'
 import DataTable from 'react-data-table-component'
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../Layout/Loader';
 
 const PreviewAllProducts = () => {
+    const { products, isLoading } = useSelector((state) => state.products);
+    const { seller } = useSelector((state) => state.seller);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllProductsShop(seller._id));
+    }, [dispatch]);
+
     const columns = [
+        { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
         {
-            name: 'Title',
+            field: 'partNumber',
+            name: 'Part Number',
+            selector: row => row.partNumber,
+            sortable: true,
+        },
+        {
+            field: "name",
+            name: "Name",
             selector: row => row.name,
+            minWidth: 180,
+            flex: 1.4,
             sortable: true,
         },
         {
-            name: 'Email',
-            selector: row => row.email,
+            field: "price",
+            name: "Price",
+            selector: row => row.price,
+            minWidth: 100,
+            flex: 0.6,
             sortable: true,
         },
         {
-            name:'Age',
-            selector: row => row.age,
-            sortable: true,
-        }
+            field: "Stock",
+            name: "Stock",
+            type: "number",
+            selector: row => row.stock,
+            minWidth: 80,
+            flex: 0.5,
+        },
+
+        {
+            field: "sold",
+            name: "Sold out",
+            type: "number",
+            selector: row => row.sold,
+            minWidth: 130,
+            flex: 0.6,
+        },
+        {
+            field: "Preview",
+            flex: 0.8,
+            minWidth: 100,
+            name: "",
+            type: "number",
+            sortable: false,
+            renderCell: (params) => {
+              return (
+                <>
+                  <Link to={`/product/${params.id}`}>
+                    <Button>
+                      <AiOutlineEye size={20} />
+                    </Button>
+                  </Link>
+                </>
+              );
+            },
+          },
     ];
+    const row = [];
     const data = [
         {
             id: 1,
@@ -81,6 +137,17 @@ const PreviewAllProducts = () => {
             age: '23'
         },
     ]
+    products &&
+    products.forEach((item) => {
+      row.push({
+        id: item._id,
+        partNumber: item.partNumber,
+        name: item.name,
+        price: "US$ " + item.discountPrice,
+        Stock: item.stock,
+        sold: item?.sold_out,
+      });
+    });
     const [records, setRecords] = useState(data);
 
     function handleFilter(event) {
@@ -91,15 +158,23 @@ const PreviewAllProducts = () => {
     }
   return (
     <>
-    <div className="text-end"><input type="text" onChange={handleFilter} /></div>
-        <DataTable
-        columns={columns}
-        data={records}
-        fixedHeader
-        pagination
-        >
+    {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full mx-8 pt-1 mt-10 bg-white">
+            <div className="text-end"><input type="text" onChange={handleFilter} /></div>
+            <DataTable
+            row={row}
+            columns={columns}
+            // data={records}
+            fixedHeader
+            pagination
+            >
 
-        </DataTable>
+            </DataTable>
+        </div>
+        )
+      }
     </>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
         <BestDeals />
         {/* <Events /> */}
         <FeaturedProduct />
-        <PreviewAllProducts/>
+        {/* <PreviewAllProducts/> */}
         <Sponsored />
         <Footer />
     </div>
