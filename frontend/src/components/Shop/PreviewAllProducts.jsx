d73e34b81b615f684b50b8c4e79571430f2e0b88
Collins77 <collinsmuemah@gmail.com>
2023-09-12 14:36:08 +0300

    updates

--- frontend/src/components/Shop/PreviewAllProducts.jsx
+++ frontend/src/components/Shop/PreviewAllProducts.jsx
@@ -1,177 +1,115 @@
-import React, { useEffect, useState } from 'react'
-import DataTable from 'react-data-table-component'
-import { useDispatch, useSelector } from 'react-redux';
-import Loader from '../Layout/Loader';
-import { getAllProductsShop } from '../../redux/actions/product';
-import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
-import { AiOutlineEye } from 'react-icons/ai';
+// ProductTable.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useTable } from 'react-table';
+import { server } from '../../server';
 
-const PreviewAllProducts = ({data}) => {
-    
-    const columns = [
-        // { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
-        {
-            field: 'partNumber',
-            name: 'Part Number',
-            selector: row => row.partNumber,
-            sortable: true,
-        },
-        {
-            field: "name",
-            name: "Name",
-            selector: row => row.name,
-            minWidth: 180,
-            flex: 1.4,
-            sortable: true,
-        },
-        {
-            field: "price",
-            name: "Price",
-            selector: row => row.price,
-            minWidth: 100,
-            flex: 0.6,
-            sortable: true,
-        },
-        {
-            field: "Stock",
-            name: "Stock",
-            type: "number",
-            selector: row => row.stock,
-            minWidth: 80,
-            flex: 0.5,
-        },
+function PreviewAllProducts({ id }) {
+  const [products, setProducts] = useState([]);
 
-        {
-            field: "sold",
-            name: "Sold out",
-            type: "number",
-            selector: row => row.sold,
-            minWidth: 130,
-            flex: 0.6,
-        },
-        {
-            field: "Preview",
-            flex: 0.8,
-            minWidth: 100,
-            name: "",
-            type: "number",
-            sortable: false,
-            renderCell: (params) => {
-              return (
-                <>
-                  <Link to={`/product/${params.id}`}>
-                    <Button>
-                      <AiOutlineEye size={20} />
-                    </Button>
-                  </Link>
-                </>
-              );
-            },
-          },
-    ];
-    const row = [];
-    // const data = [
-    //     {
-    //         id: 1,
-    //         name: 'yousaf',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'collins',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 3,
-    //         name: 'jimmy',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 4,
-    //         name: 'kamau',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 5,
-    //         name: 'omwami',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 1,
-    //         name: 'yousaf',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'collins',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 3,
-    //         name: 'jimmy',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 4,
-    //         name: 'kamau',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    //     {
-    //         id: 5,
-    //         name: 'omwami',
-    //         email: 'yousaf@gmail.com',
-    //         age: '23'
-    //     },
-    // ]
-    // products &&
-    // products.forEach((item) => {
-    //   row.push({
-    //     // id: item._id,
-    //     partNumber: item.partNumber,
-    //     name: item.name,
-    //     price: "US$ " + item.discountPrice,
-    //     Stock: item.stock,
-    //     sold: item?.sold_out,
-    //   });
-    // });
-    const [records, setRecords] = useState(data);
+  const columns = [
+    { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 180,
+      flex: 1.4,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      minWidth: 100,
+      flex: 0.6,
+    },
+    {
+      field: "Stock",
+      headerName: "Stock",
+      type: "number",
+      minWidth: 80,
+      flex: 0.5,
+    },
 
-    function handleFilter(event) {
-        const newData = data.filter(row => {
-            return row.name.toLowerCase().includes(event.target.value.toLowerCase())
-        })
-        setRecords(newData)
-    }
+    {
+      field: "sold",
+      headerName: "Sold out",
+      type: "number",
+      minWidth: 130,
+      flex: 0.6,
+    },
+    {
+      field: "Preview",
+      flex: 0.8,
+      minWidth: 100,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/product/${params.id}`}>
+              <Button>
+                <AiOutlineEye size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+    // Add more columns as needed
+  ];
+
+  useEffect(() => {
+    // Fetch products for a particular shop from your server
+    axios.get(`${server}/product/get-all-products-shop/${id}`)
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }, [id]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data: products,
+  });
+
   return (
-    <>
-   
-        <div className="w-full mx-8 pt-1 mt-10 bg-white">
-            <div className="text-end"><input 
-            type="text" 
-            onChange={handleFilter} 
-            /></div>
-            <DataTable
-            row={row}
-            columns={columns}
-            data={records}
-            fixedHeader
-            pagination
-            >
-
-            </DataTable>
-        </div>
-    </>
-  )
+    <div>
+      <h2>Products of Shop {id}</h2>
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default PreviewAllProducts
+export default PreviewAllProducts;
