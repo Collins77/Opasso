cc298036216c03ed756684ea7f54caea3692c91a
Collins77 <collinsmuemah@gmail.com>
2023-09-13 09:46:29 +0300

    updates

--- frontend/src/components/Shop/PreviewAllProducts.jsx
+++ frontend/src/components/Shop/PreviewAllProducts.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTable } from 'react-table';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const PreviewAllProducts = () => {
+    const [data,setData] = useState({});
+    const {products} = useSelector((state) => state.products);
+    const [isLoading,setIsLoading] = useState(false);
+    const {id} = useParams();
+    const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProductsShop(id));
+    setIsLoading(true);
+    axios.get(`${server}/shop/preview/${id}`).then((res) => {
+     setData(res.data.shop);
+     setIsLoading(false);
+    }).catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+    })
+  }, [])
+
+  const columns = React.useMemo(
+    () => [
+        { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
+        {
+          field: "name",
+          headerName: "Name",
+          minWidth: 180,
+          flex: 1.4,
+        },
+        {
+          field: "price",
+          headerName: "Price",
+          minWidth: 100,
+          flex: 0.6,
+        },
+        {
+          field: "Stock",
+          headerName: "Stock",
+          type: "number",
+          minWidth: 80,
+          flex: 0.5,
+        },
+    
+        {
+          field: "sold",
+          headerName: "Sold out",
+          type: "number",
+          minWidth: 130,
+          flex: 0.6,
+        },
+      // Add more columns as needed
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  });
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
   return (
-    <div>PreviewAllProducts</div>
-  )
+    <div>
+      <h2>Shop Data Table</h2>
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default PreviewAllProducts
