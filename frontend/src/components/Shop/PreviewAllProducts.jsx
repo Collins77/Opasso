2783f4c1cd70aa9ceb9086b9f5efb8e80e5c4b05
Collins77 <collinsmuemah@gmail.com>
2023-09-13 09:58:29 +0300

    updates

--- frontend/src/components/Shop/PreviewAllProducts.jsx
+++ frontend/src/components/Shop/PreviewAllProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTable } from 'react-table';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
