af3e01428d3d40bd71932a329e6973cc4655a48b
Collins77 <collinsmuemah@gmail.com>
2023-10-13 07:14:11 +0300

    updates

--- frontend/src/components/Shop/PreviewAllProducts.jsx
+++ frontend/src/components/Shop/PreviewAllProducts.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { DataGrid } from "@material-ui/data-grid";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getAllProductsShop } from "../../redux/actions/product";
 import Loader from "../Layout/Loader";
-import { Button } from "@material-ui/core";
+import { Button, Checkbox, MenuItem, Select } from "@material-ui/core";
 import { AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import MaterialTable from 'material-table';
+import { categoriesData } from "../../static/data";
+
 
 const PreviewAllProducts = () => {
   const { products, isLoading } = useSelector((state) => state.products);
   const { id } = useParams();
+  const [filteredData, setFilteredData] = useState(products);
+  const [filter, setFilter] = useState(true);
+  const [category, setCategory] = useState('all');
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllProductsShop(id));
   }, [dispatch, id]);
 
-  const [filteredProducts, setFilteredProducts] = useState(products);
 
   const columns = [
     { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
@@ -40,17 +44,13 @@ const PreviewAllProducts = () => {
       minWidth: 80,
       flex: 0.5,
     },
+
     {
       field: "category",
       headerName: "Category",
+      type: "number",
       minWidth: 130,
       flex: 0.6,
-      filterOperators: ["equals"],
-      filterOperator: "equals",
-      valueGetter: (params) => params.row.category,
-      renderCell: (params) => {
-        return <span>{params.value}</span>;
-      },
     },
     {
       field: "Preview",
@@ -73,33 +73,66 @@ const PreviewAllProducts = () => {
     },
   ];
 
-  const rows = products?.map((item) => ({
-    id: item._id,
-    name: item.name,
-    price: `US$ ${item.discountPrice}`,
-    Stock: item.stock,
-    category: item.category,
-  }));
+  const handleChange = () => {
+    setFilter(!filter)
+  }
 
-  
-
+  useEffect(() => {
+    setFilteredData(category === 'all' ? products : products.filter(item=>item.category===category))
+  },[category, products])
+ 
+ 
   return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div className="w-full mx-8 pt-1 mt-10 bg-white">
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={10}
-            disableSelectionOnClick
-            autoHeight
-          />
-        </div>
-      )}
-    </>
-  );
+   <>
+    {isLoading ? (
+      <Loader />
+    ) : (
+      <div className="w-full mx-8 pt-1 mt-10 bg-white">
+        
+        <MaterialTable
+          title="Products"
+          data={filteredData}
+          columns={columns}
+          options={{
+            filtering:filter
+          }}
+          actions={[
+            {
+              icon: () =><Checkbox
+              checked={filter}
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+              />,
+              tooltip: "Hide/Show Filter Options",
+              isFreeAction: true,
+            },
+            {
+              icon: () => <Select
+              labelId="select-label"
+              id="simple-select"
+              style={{width:100}}
+              value={category}
+              onChange={(e) =>setCategory(e.target.value)}
+              > 
+              <MenuItem value={'all'}>All</MenuItem>
+              {categoriesData &&
+                  categoriesData.map((i) => {
+                    return (
+                      <MenuItem value={category}>{i.title}</MenuItem>
+                    )
+                  })
+                } 
+              </Select>,
+              tooltip: "Filter Categories",
+              isFreeAction: true,
+            }
+          ]}
+        />
+      </div>
+    )}
+   </> 
+  )
+
 };
 
 export default PreviewAllProducts;
