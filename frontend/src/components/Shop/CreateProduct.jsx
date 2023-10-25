209cc86a3d269815cad1d2145336f3e300274970
Collins77 <collinsmuemah@gmail.com>
2023-10-25 12:28:23 +0300

    updates

--- frontend/src/components/Shop/CreateProduct.jsx
+++ frontend/src/components/Shop/CreateProduct.jsx
@@ -201,7 +201,7 @@ const CreateProduct = () => {
             name="isAvailable"
             // value={isAvailable}
             checked={isAvailable}
-            // className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            className="ml-2"
             onChange={(e) => setIsAvailable(e.target.value)}
           />
         </div>
