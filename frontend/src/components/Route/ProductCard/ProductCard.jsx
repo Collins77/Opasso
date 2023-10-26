be603ddc6fea41d3bdd00f055e29237083aa2d87
Collins77 <collinsmuemah@gmail.com>
2023-10-26 08:20:26 +0300

    updates

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -86,17 +86,17 @@ const ProductCard = ({ data,isEvent }) => {
           <div className="py-2 flex items-center justify-between">
             <div className="flex">
               <h5 className={`${styles.productDiscountPrice}`}>
+                KES
                 {data.originalPrice === 0
                   ? data.originalPrice
-                  : data.discountPrice}
-                $
+                  : data.discountPrice} 
               </h5>
               {/* <h4 className={`${styles.price}`}>
                 {data.originalPrice ? data.originalPrice + " $" : null}
               </h4> */}
             </div>
-            <span className="font-[400] text-[17px] text-[#68d284]">
-              {data?.sold_out} stock
+            <span className="font-[400] text-[12px] text-[#68d284]">
+              Available
             </span>
           </div>
         </Link>
