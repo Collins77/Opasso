c67f5220846c998c27cc22ffd68ebca8042ad708
Collins77 <collinsmuemah@gmail.com>
2023-10-27 14:00:26 +0300

    updates

--- backend/model/event.js
+++ backend/model/event.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
+    
     name:{
         type: String,
         required:[true,"Please enter your event product name!"],
