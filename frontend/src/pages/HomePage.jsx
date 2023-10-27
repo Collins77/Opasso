a558dc736cb5013bf0b718f735f3bff7cbca1745
Collins77 <collinsmuemah@gmail.com>
2023-10-27 13:53:22 +0300

    updates

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 // import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
-// import Events from "../components/Events/Events";
+import Events from "../components/Events/Events";
 import Sponsored from "../components/Route/Sponsored";
 import Footer from "../components/Layout/Footer";
 // import PreviewAllProducts from '../components/Shop/PreviewAllProducts';
@@ -16,7 +16,7 @@ const HomePage = () => {
         <Hero />
         <Categories />
         <BestDeals />
-        {/* <Events /> */}
+        <Events />
         {/* <FeaturedProduct /> */}
         {/* <PreviewAllProducts id={'64da79b4cad5b45ae8579322'}/> */}
         <Sponsored />
