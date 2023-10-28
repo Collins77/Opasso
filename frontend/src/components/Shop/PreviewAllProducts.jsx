54e2648f852235018bc09d544fc3569fa9c627a3
Collins77 <collinsmuemah@gmail.com>
2023-10-28 11:08:01 +0300

    updates

--- frontend/src/components/Shop/PreviewAllProducts.jsx
+++ frontend/src/components/Shop/PreviewAllProducts.jsx
@@ -1,4 +1,4 @@
-import { Button, FormControlLabel, MenuItem, Select, Switch, makeStyles } from "@material-ui/core";
+import { Button, FormControlLabel, MenuItem, Select, Switch, makeStyles, styled } from "@material-ui/core";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 import { AiOutlineEye } from "react-icons/ai";
@@ -11,6 +11,48 @@ const PreviewAllProducts = () => {
   const { products, isLoading } = useSelector((state) => state.products);
   const { id } = useParams();
   const [selectedCurrency, setSelectedCurrency] = useState("KES"); 
+
+  const AntSwitch = styled(Switch)(({ theme }) => ({
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+    '&:active': {
+      '& .MuiSwitch-thumb': {
+        width: 15,
+      },
+      '& .MuiSwitch-switchBase.Mui-checked': {
+        transform: 'translateX(9px)',
+      },
+    },
+    '& .MuiSwitch-switchBase': {
+      padding: 2,
+      '&.Mui-checked': {
+        transform: 'translateX(12px)',
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+      width: 12,
+      height: 12,
+      borderRadius: 6,
+      transition: theme.transitions.create(['width'], {
+        duration: 200,
+      }),
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: 16 / 2,
+      opacity: 1,
+      backgroundColor:
+        theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+      boxSizing: 'border-box',
+    },
+  }));
 
 
   const useStyles = makeStyles((theme) => ({
@@ -50,13 +92,20 @@ const PreviewAllProducts = () => {
       field: "price",
       headerName: (
         <div>
-          <Switch
+          {/* <Switch
              checked={selectedCurrency === "USD"}
              onChange={handleCurrencyChange}
              name="currencySwitch"
              inputProps={{"aria-label": "Currency Switch"}}
              />
-             {selectedCurrency === "USD" ? "USD" : "KES"}
+             {selectedCurrency === "USD" ? "USD" : "KES"} */}
+            <AntSwitch
+            checked={selectedCurrency === "USD"}
+            onChange={handleCurrencyChange}
+            name="currencySwitch"
+            inputProps={{ "aria-label": "Currency Switch" }}
+            />
+            {selectedCurrency === "USD" ? "USD" : "KES"}
         </div>
       ),
       headerClassName:
