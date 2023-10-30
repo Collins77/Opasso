dea4ca09b26878abc806a399107a3d83da7c7588
Collins77 <collinsmuemah@gmail.com>
2023-10-30 13:16:30 +0300

    updates

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -1,40 +1,44 @@
-import React, { useEffect } from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
-import styles from '../../styles/styles'
-import EventCard from "./EventCard";
+import styles from '../../styles/styles';
+import EventCard from './EventCard';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Events = () => {
-  const {allEvents,isLoading} = useSelector((state) => state.events);  
-   
+  const { allEvents, isLoading } = useSelector((state) => state.events);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, // Adjust the number of cards shown at once
+    slidesToScroll: 1,
+  };
+
   return (
     <div>
-     {
-      !isLoading && (
+      {!isLoading && (
         <div className={`${styles.section}`}>
-      <div className={`${styles.heading}`}>
-        <h1>Popular Events</h1>
-      </div>
-
-      <div className="w-full grid">
-         {
-          allEvents.length !== 0 && (
-            <EventCard data={allEvents && allEvents[0]} />
-          )
-         }
-         <h4>{
-           allEvents?.length === 0 && (
-            'Event Info not available!'
-           )
-          }
-
-         </h4>
-      </div>
-     
+          <div className={`${styles.heading}`}>
+            <h1>Popular Events</h1>
+          </div>
+          {allEvents.length > 0 ? (
+            <Slider {...settings}>
+              {allEvents.map((event, index) => (
+                <div key={index}>
+                  <EventCard data={event} active={true} />
+                </div>
+              ))}
+            </Slider>
+          ) : (
+            <h4>'Event Info not available!'</h4>
+          )}
+        </div>
+      )}
     </div>
-      )
-     }
-  </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
