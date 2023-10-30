b996f6025ded1ec46fa157ce69a5cb82448d9a27
Collins77 <collinsmuemah@gmail.com>
2023-10-30 12:21:21 +0300

    updates

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -60,22 +60,30 @@ const EventCard = ({ active, data }) => {
     //   </div>
     // </div>
     <div
-  class="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] h-[200px] mb-3 dark:bg-neutral-700 md:max-w-xl md:flex-row">
-  <img
+  class="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] h-[300px] mb-5 dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  {/* <img
     class="h-50 w-full rounded-t-lg object-cover md:h-auto md:!rounded-none md:!rounded-l-lg"
     src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-    alt="" />
+    alt="" /> */}
+    <img
+      className="h-50 w-full rounded-t-lg object-cover md:h-auto md:!rounded-none md:!rounded-l-lg"
+      src={`${data.images[0]?.url}`} 
+      alt="" 
+     />
   <div class="flex flex-col justify-start p-6">
     <h5
       class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-      Card title
+      {data.title}
+    </h5>
+    <h5
+      class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+      {data.name}
     </h5>
     <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-      This is a wider card with supporting text below as a natural lead-in
-      to additional content. This content is a little bit longer.
+      {data.description}
     </p>
     <p class="text-xs text-neutral-500 dark:text-neutral-300">
-      Last updated 3 mins ago
+      <CountDown data={data} />
     </p>
   </div>
 </div>
