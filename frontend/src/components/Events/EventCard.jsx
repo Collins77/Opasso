c160339cd75def94f3c931ef6707cb95cc9b59f9
Collins77 <collinsmuemah@gmail.com>
2023-11-02 09:57:08 +0300

    updates

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -9,93 +9,19 @@ import { toast } from "react-toastify";
 
 
 const EventCard = ({ active, data }) => {
-  // const { cart } = useSelector((state) => state.cart);
-  // const dispatch = useDispatch();
-
-  // const addToCartHandler = (data) => {
-  //   const isItemExists = cart && cart.find((i) => i._id === data._id);
-  //   if (isItemExists) {
-  //     toast.error("Item already in cart!");
-  //   } else {
-  //     if (data.stock < 1) {
-  //       toast.error("Product stock limited!");
-  //     } else {
-  //       const cartData = { ...data, qty: 1 };
-  //       dispatch(addTocart(cartData));
-  //       toast.success("Item added to cart successfully!");
-  //     }
-  //   }
-  // }
+  const backgroundImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Flovepik.com%2Fimages%2Fpromotion-background.html&psig=AOvVaw39zn5-643BbanD9LCf4WwX&ust=1698994489136000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCIDVsJDepIIDFQAAAAAdAAAAABAE"
+  
   return (
-    // <div
-    //   className={`w-full block bg-white rounded-lg ${
-    //     active ? "unset" : "mb-12"
-    //   } lg:flex p-2`}
-    // >
-    //   <div className="w-full lg:-w[50%] m-auto">
-    //     <img src={`${data.images[0]?.url}`} alt="" />
-    //   </div>
-    //   <div className="w-full lg:[w-50%] flex flex-col justify-center">
-    //     <h2 className={`${styles.productTitle}`}>{data.name}</h2>
-    //     <p>{data.description}</p>
-    //     <div className="flex py-2 justify-between">
-    //       <div className="flex">
-    //         <h5 className="font-[500] text-[18px] text-[#d55b45] pr-3 line-through">
-    //           KES {data.originalPrice}
-    //         </h5>
-    //         <h5 className="font-bold text-[20px] text-[#333] font-Roboto">
-    //           KES {data.discountPrice}
-    //         </h5>
-    //       </div>
-    //       <span className="pr-3 font-[400] text-[17px] text-[#44a55e]">
-    //         In Stock
-    //       </span>
-    //     </div>
-    //     <CountDown data={data} />
-    //     <br />
-    //     <div className="flex items-center">
-    //       <Link to={`/product/${data._id}?isEvent=true`}>
-    //         <div className={`${styles.button} text-[#fff]`}>See Details</div>
-    //       </Link>
-    //       {/* <div className={`${styles.button} text-[#fff] ml-5`} onClick={() => addToCartHandler(data)}>Add to cart</div> */}
-    //     </div>
-    //   </div>
-    // </div>
-//     <div
-//   className={`flex flex-row w-[80%] p-10 
-//   mx-auto mb-12 ${active ? 'w-4/5' : 'w-2/5'}
-//   rounded-lg 
-//   bg-neutral-50
-//   shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] h-[300px] mb-5`}>
-//     <img
-//       className="h-full w-[45%] object-contain"
-//       src={`${data.images[0]?.url}`} 
-//       alt="" 
-//      />
-//   <div class="h-full w-[55%] flex flex-col justify-start">
-//     <h3
-//       class="mb-2 text-sm font-semibold text-[#c23b01]">
-//       {data.title}
-//     </h3>
-//     <h4
-//       class="mb-2 text-sm font-semibold text-[#c23b01]">
-//       {data.event_description}
-//     </h4>
-//     <h2
-//       class="mb-2 text-xl font-medium text-neutral-800">
-//       {data.name}
-//     </h2>
-//     <p class="mb-4 text-base text-neutral-60">
-//       {data.description}
-//     </p>
-//     <p class="text-xs text-neutral-500">
-//       <CountDown data={data} />
-//     </p>
-//   </div>
-// </div>
+    
 
 <div>
-      <div className="bg-neutral-200 text-white rounded shadow-xl py-5 px-5 w-full h-[200px] flex items-center justify-evenly">
+      <div 
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+      className="bg-neutral-200 text-white rounded shadow-xl py-5 px-5 w-full h-[200px] flex items-center justify-evenly">
         <div className="flex flex-wrap -mx-3 items-center">
           <div className="w-1/4 px-3 text-center hidden md:block">
           <svg height="150px" width="150px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve">
