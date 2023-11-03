2ea080324f7c23c9156c7e0f15005035118ea972
Collins77 <collinsmuemah@gmail.com>
2023-11-03 20:33:39 +0300

    updates

--- frontend/src/components/AppPromo/appPromo.jsx
+++ frontend/src/components/AppPromo/appPromo.jsx
@@ -3,7 +3,7 @@ import styles from '../../styles/styles';
 
 const AppPromo = () => {
   return (
-    <div className={`${styles.section} w-full p-4 mb-12 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8`}>
+    <div className={`${styles.section} p-4 mb-12 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8`}>
       <h5 className="mb-2 text-3xl font-bold text-gray-900">Work fast from anywhere</h5>
       <p className="mb-5 text-base text-gray-500 sm:text-lg">
         Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.
