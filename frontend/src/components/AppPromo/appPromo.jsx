f6c64ffb0a6cc79c7f1baf10785293891df9fa31
Collins77 <collinsmuemah@gmail.com>
2023-11-03 20:37:19 +0300

    updates

--- frontend/src/components/AppPromo/appPromo.jsx
+++ frontend/src/components/AppPromo/appPromo.jsx
@@ -6,7 +6,7 @@ const AppPromo = () => {
     <div className={`${styles.section} p-4 mb-12 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8`}>
       <h5 className="mb-2 text-3xl font-bold text-gray-900">Work fast from anywhere</h5>
       <p className="mb-5 text-base text-gray-500 sm:text-lg">
-        Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.
+        Stay up to date and move work forward with Opasso on iOS & Android. Download the app today.
       </p>
       <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
         <a href="#" className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5">
