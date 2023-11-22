0dc9d57de451836553bb58df090b1ac759c4bfcb
Collins77 <collinsmuemah@gmail.com>
2023-11-22 19:45:21 +0300

    updates

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,8 +16,8 @@ const HomePage = () => {
         <Header activeHeading={1} />
         <Hero />
         <Categories />
-        <BestDeals />
-        <Events />
+        {/* <BestDeals /> */}
+        {/* <Events /> */}
         {/* <Sponsored /> */}
         <AppPromo />
         <Footer />
