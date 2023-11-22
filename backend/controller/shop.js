f81723831c8ce38d5d876812a03837a4aec8ae96
Collins77 <collinsmuemah@gmail.com>
2023-11-22 20:50:02 +0300

    updates

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -19,93 +19,74 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
       return next(new ErrorHandler("User already exists", 400));
     }
 
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-      folder: "avatars",
-    });
-
-
     const seller = {
       name: req.body.name,
-      email: email,
+      email,
       password: req.body.password,
-      avatar: {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url,
-      },
       address: req.body.address,
       phoneNumber: req.body.phoneNumber,
-      zipCode: req.body.zipCode,
+      status: "Not approved", // Set the status to "Not approved" by default
     };
 
-    const activationToken = createActivationToken(seller);
+    const newSeller = await Shop.create(seller);
 
-    const activationUrl = `https://opasso-frontend.vercel.app/seller/activation/${activationToken}`;
-
-    try {
-      await sendMail({
-        email: seller.email,
-        subject: "Activate your Shop",
-        message: `Hello ${seller.name}, please click on the link to activate your shop: ${activationUrl}`,
-      });
-      res.status(201).json({
-        success: true,
-        message: `please check your email:- ${seller.email} to activate your shop!`,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
+    res.status(201).json({
+      success: true,
+      message: "User created successfully. Waiting for admin approval.",
+      user: newSeller,
+    });
   } catch (error) {
     return next(new ErrorHandler(error.message, 400));
   }
 }));
 
 // create activation token
-const createActivationToken = (seller) => {
-  return jwt.sign(seller, process.env.ACTIVATION_SECRET, {
-    expiresIn: "24h",
-  });
-};
+// const createActivationToken = (seller) => {
+//   return jwt.sign(seller, process.env.ACTIVATION_SECRET, {
+//     expiresIn: "24h",
+//   });
+// };
 
-// activate user
-router.post(
-  "/activation",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const { activation_token } = req.body;
+// // activate user
+// router.post(
+//   "/activation",
+//   catchAsyncErrors(async (req, res, next) => {
+//     try {
+//       const { activation_token } = req.body;
 
-      const newSeller = jwt.verify(
-        activation_token,
-        process.env.ACTIVATION_SECRET
-      );
+//       const newSeller = jwt.verify(
+//         activation_token,
+//         process.env.ACTIVATION_SECRET
+//       );
 
-      if (!newSeller) {
-        return next(new ErrorHandler("Invalid token", 400));
-      }
-      const { name, email, password, avatar, zipCode, address, phoneNumber } =
-        newSeller;
+//       if (!newSeller) {
+//         return next(new ErrorHandler("Invalid token", 400));
+//       }
+//       const { name, email, password, avatar, zipCode, address, phoneNumber } =
+//         newSeller;
 
-      let seller = await Shop.findOne({ email });
+//       let seller = await Shop.findOne({ email });
 
-      if (seller) {
-        return next(new ErrorHandler("User already exists", 400));
-      }
+//       if (seller) {
+//         return next(new ErrorHandler("User already exists", 400));
+//       }
 
-      seller = await Shop.create({
-        name,
-        email,
-        avatar,
-        password,
-        zipCode,
-        address,
-        phoneNumber,
-      });
+//       seller = await Shop.create({
+//         name,
+//         email,
+//         avatar,
+//         password,
+//         zipCode,
+//         address,
+//         phoneNumber,
+//       });
 
-      sendShopToken(seller, 201, res);
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
-);
+//       sendShopToken(seller, 201, res);
+//     } catch (error) {
+//       return next(new ErrorHandler(error.message, 500));
+//     }
+//   })
+// );
 
 // login shop
 router.post(
@@ -115,13 +96,13 @@ router.post(
       const { email, password } = req.body;
 
       if (!email || !password) {
-        return next(new ErrorHandler("Please provide the all fields!", 400));
+        return next(new ErrorHandler("Please provide all fields!", 400));
       }
 
       const user = await Shop.findOne({ email }).select("+password");
 
       if (!user) {
-        return next(new ErrorHandler("User doesn't exists!", 400));
+        return next(new ErrorHandler("User doesn't exist!", 400));
       }
 
       const isPasswordValid = await user.comparePassword(password);
@@ -130,6 +111,11 @@ router.post(
         return next(
           new ErrorHandler("Please provide the correct information", 400)
         );
+      }
+
+      // Check if the user is approved before sending the token
+      if (user.status !== "Approved") {
+        return next(new ErrorHandler("Your account is not yet approved.", 401));
       }
 
       sendShopToken(user, 201, res);
--- backend/model/shop.js
+++ backend/model/shop.js
@@ -32,46 +32,11 @@ const shopSchema = new mongoose.Schema({
     type: String,
     default: "Seller",
   },
-  avatar: {
-    public_id: {
-      type: String,
-      required: true,
-    },
-    url: {
-      type: String,
-      required: true,
-    },
+  status: {
+    type: String,
+    enum: ["Not approved", "Approved"],
+    default: "Not approved",
   },
-  zipCode: {
-    type: Number,
-    required: true,
-  },
-  withdrawMethod: {
-    type: Object,
-  },
-  availableBalance: {
-    type: Number,
-    default: 0,
-  },
-  transections: [
-    {
-      amount: {
-        type: Number,
-        required: true,
-      },
-      status: {
-        type: String,
-        default: "Processing",
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now(),
-      },
-      updatedAt: {
-        type: Date,
-      },
-    },
-  ],
   createdAt: {
     type: Date,
     default: Date.now(),
