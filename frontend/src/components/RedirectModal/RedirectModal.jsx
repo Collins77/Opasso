24e5c03cec0f8a2b55d98e334e51ce9af10a6541
Collins77 <collinsmuemah@gmail.com>
2023-11-24 11:34:25 +0300

    updates

--- frontend/src/components/RedirectModal/RedirectModal.jsx
+++ frontend/src/components/RedirectModal/RedirectModal.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
 import { RxCross1 } from 'react-icons/rx';
 import styles from '../../styles/styles';
+import { useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 
 
 const RedirectModal = ({ isOpen, onClose, message }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleConfirm = ({isOpen, onClose, message}) => {
+    // Close the modal (implement this based on your requirements)
+    onClose();
+
+    // Redirect to the login page
+    history.push("/login");
+  };
+
+  const handleClose = () => {
+    // Close the modal (implement this based on your requirements)
+    onClose();
+  };
+
   return (
     <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
             <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
               <div className="w-full flex justify-end cursor-pointer">
-                {/* <RxCross1 size={25} onClick={() => setOpen(false)} /> */}
+              <RxCross1 size={25} onClick={handleClose} />
               </div>
               <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
-                Are you sure you wanna delete this seller?
+                {message}
               </h3>
               <div className="w-full flex items-center justify-center">
                 <div
                   className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
-                  // onClick={() => setOpen(false)}
+                  onClick={handleClose}
                 >
                   cancel
                 </div>
                 <div
                   className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
-                  // onClick={() =>  setOpen(false) || handleDelete(userId)}
+                  onClick={handleConfirm}
                 >
                   confirm
                 </div>
--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -1,13 +1,23 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import RedirectModal from "../components/RedirectModal/RedirectModal";
+import { useState } from "react";
 
 const ProtectedRoute = ({ children }) => {
   const { loading, isAuthenticated } = useSelector((state) => state.user);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
   if (loading === false) {
     if (!isAuthenticated) {
       // return <Navigate to="/login" replace />;
-      return <RedirectModal />
+      return <RedirectModal
+      isOpen={isModalOpen}
+      onClose={handleModalClose}
+      message="You need to be logged in to access this page. Do you want to go to the login page?"
+    />
     }
     return children;
   }
