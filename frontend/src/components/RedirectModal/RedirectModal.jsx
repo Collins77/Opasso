3b33695ddcc281613eb0587e17f62554faa0824b
Collins77 <collinsmuemah@gmail.com>
2023-11-24 11:20:49 +0300

    updates

--- frontend/src/components/RedirectModal/RedirectModal.jsx
+++ frontend/src/components/RedirectModal/RedirectModal.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import Modal from 'react-modal';
-import App from '../../App'; // Replace with the path to your main App component
-import ReactDOM from "react-dom";
+import { RxCross1 } from 'react-icons/rx';
+import styles from '../../styles/styles';
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-Modal.setAppElement("#root");
 
 const RedirectModal = ({ isOpen, onClose, message }) => {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose}>
-      <div>
-        <p>{message}</p>
-        <button onClick={onClose}>OK</button>
-      </div>
-    </Modal>
+    <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+            <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+              <div className="w-full flex justify-end cursor-pointer">
+                {/* <RxCross1 size={25} onClick={() => setOpen(false)} /> */}
+              </div>
+              <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+                Are you sure you wanna delete this seller?
+              </h3>
+              <div className="w-full flex items-center justify-center">
+                <div
+                  className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
+                  // onClick={() => setOpen(false)}
+                >
+                  cancel
+                </div>
+                <div
+                  className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
+                  // onClick={() =>  setOpen(false) || handleDelete(userId)}
+                >
+                  confirm
+                </div>
+              </div>
+            </div>
+          </div>
+    
   );
 };
 
--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import RedirectModal from "../components/RedirectModal/RedirectModal";
 
 const ProtectedRoute = ({ children }) => {
   const { loading, isAuthenticated } = useSelector((state) => state.user);
   if (loading === false) {
     if (!isAuthenticated) {
-      return <Navigate to="/login" replace />;
+      // return <Navigate to="/login" replace />;
+      return <RedirectModal />
     }
     return children;
   }
