09ca3fe9c42adaeba3d993d0a663a701415f3e6e
Collins77 <collinsmuemah@gmail.com>
2023-11-24 12:04:57 +0300

    updates

--- frontend/src/components/RedirectModal/RedirectModal.jsx
+++ frontend/src/components/RedirectModal/RedirectModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RxCross1 } from 'react-icons/rx';
 import styles from '../../styles/styles';
-import { Navigate, useHistory } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 
 
