fbd86bf67a2c73052a7b48d71f228905bf5a801c
Collins77 <collinsmuemah@gmail.com>
2023-11-25 08:14:55 +0300

    updates

--- frontend/src/components/RedirectModal/RedirectModal.jsx
+++ frontend/src/components/RedirectModal/RedirectModal.jsx
@@ -32,7 +32,7 @@ const RedirectModal = ({ isOpen, onClose, message }) => {
                       <svg class="mx-auto mb-4 text-red-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                       </svg>
-                      <span class="text-2xl font-medium">Payment Successful</span>
+                      <span class="text-2xl font-medium">Restricted Access</span>
                       <h3 class="mb-5 text-lg font-normal text-gray-500">{message}</h3>
                       
                       <Link 
