5a614bda4213bc8d75760d57fc5d13337ac6329d
Collins77 <collinsmuemah@gmail.com>
2023-12-12 17:56:58 +0300

    updates

--- frontend/src/pages/Shop/ShopPreviewPage.jsx
+++ frontend/src/pages/Shop/ShopPreviewPage.jsx
@@ -12,17 +12,18 @@ const ShopPreviewPage = () => {
     <>
       <Header />
       <div className={`${styles.section} bg-[#f5f5f5] mt-5`}>
-      <div className='w-full bg-orange-200 mb-3 h-[200px] flex items-center justify-center'>
-        <h1>Supplier Preview</h1>
-      </div>
-      <div className="mb-2">
-              <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0, fontSize: "12px", color: "#000000ff" }}>
-                <li><a href="/">Home/</a></li>
-                {pathnames.map((name, index) => (
-                  <li key={index}><a href={`/${name}`}>/{name}</a></li>
-                ))}
-              </ul>
-      </div>
+          <div className="mb-2">
+                  <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0, fontSize: "12px", color: "#000000ff" }}>
+                    <li><a href="/">Home/</a></li>
+                    {pathnames.map((name, index) => (
+                      <li key={index}><a href={`/${name}`}>/{name}</a></li>
+                    ))}
+                  </ul>
+          </div>
+          <div className='w-full bg-orange-200 mb-3 h-[50px] flex items-center justify-center'>
+            <h1 className='font-bold'>Supplier Preview</h1>
+          </div>
+      
          <div className="w-full flex gap-2 800px:flex py-10 justify-between">
             <div className="800px:w-[32%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-10 left-0 z-10">
               <ShopInfo isOwner={false} />
