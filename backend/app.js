20dae104593d78930013e84d46d3e325f714960b
Collins Muema <51775384+Collins77@users.noreply.github.com>
2024-01-31 10:19:23 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const path = require("path");
+const path = require("path");
 
 app.use(cors({
   origin: ['https://resellersprint.com',],
@@ -15,6 +15,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
   res.send("Hello world!");
+});
+
+const buildPath = path.join(__dirname, "build"); // Adjust the path
+
+app.use(express.static(buildPath));
+
+// Route all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 // const buildPath = path.join(__dirname, "../frontend/build");
